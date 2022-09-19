03e2f64e48956dc2a98d618ead277b2dadc6de83
angelicajulianaa <angelicajulianaa@gmail.com>
2022-09-19 23:11:36 +0800

    App.js Edited

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Varela+Round&display=swap');
+</style>
 
 function App() {
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class = "Title">
+      <h1 class = "title-text">SEXTANT</h1>
+      <p class = "title-paragraph">Sextant is a locally hosted single-page application meant to assist field technicians in their day-to-day.</p>
+      </div>
+
+      <div class = "Card1">
+        <div class = "card-body">
+          <h4 class = "card-title">Card 1</h4>
+          <h6 class = "card-subtitle">This is Card 1</h6>
+          <p class = "card-text">This is Card 1 out of 3</p>
+        </div>
+      </div>
+
+      <div class = "Card2">
+        <div class = "card-body">
+          <h4 class = "card-title">Card 2</h4>
+          <h6 class = "card-subtitle">This is Card 2</h6>
+          <p class = "card-text">This is Card 2 out of 3</p>
+        </div>
+      </div>
+
+      <div class = "Card3">
+        <div class = "card-body">
+          <h4 class = "card-title">Card 3</h4>
+          <h6 class = "card-subtitle">This is Card 3</h6>
+          <p class = "card-text">This is Card 3 out of 3</p>
+        </div>
+      </div>
+
     </div>
   );
 }
